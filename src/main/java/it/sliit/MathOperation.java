2508664238bbae2ef9752e433d51a513484f457a
JameemaFathima <it17137874@my.sliit.lk>
2021-10-15 22:07:36 +0530

    added division function

--- src/main/java/it/sliit/MathOperation.java
+++ src/main/java/it/sliit/MathOperation.java
@@ -7,12 +7,18 @@ public class MathOperation {
 
     public static double add(double... operands)
     {
+
         return Arrays.stream(operands).reduce(0d, Double::sum);
     }
 
     public static double subtract(double operand1, double operand2)
     {
         return operand1 - operand2;
+    }
+
+    public static double division(double operand1, double operand2)
+    {
+        return operand1 / operand2;
     }
 
 }
